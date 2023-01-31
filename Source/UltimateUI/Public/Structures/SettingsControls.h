7afcd7007d8a28678ca5228c00bfbe352c8d61c6
Danial Kama <dankam77.dk@gmail.com>
2023-01-31 20:05:34 +0330

    Delete invert look X option

--- Source/UltimateUI/Public/Structures/SettingsControls.h
+++ Source/UltimateUI/Public/Structures/SettingsControls.h
@@ -18,18 +18,15 @@ struct ULTIMATEUI_API FSettingsControls
 	/** True = Toggle, False = Hold */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings|Controls")
 	bool SprintMode;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings|Controls")
-	bool InvertLookX;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings|Controls")
 	bool InvertLookY;
 	
 	FSettingsControls()
-		: ForceFeedback(true), SprintMode(true), InvertLookX(false), InvertLookY(false)
+		: ForceFeedback(true), SprintMode(true), InvertLookY(false)
 	{}
 
-	FSettingsControls(const bool InForceFeedback, const bool InSprintMode, const bool InInvertLookX, const bool InInvertLookY)
-		: ForceFeedback(InForceFeedback), SprintMode(InSprintMode), InvertLookX(InInvertLookX), InvertLookY(InInvertLookY)
+	FSettingsControls(const bool InForceFeedback, const bool InSprintMode, const bool InInvertLookY)
+		: ForceFeedback(InForceFeedback), SprintMode(InSprintMode), InvertLookY(InInvertLookY)
 	{}
 };
